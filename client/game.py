6f5504d27899c7f639bb4320c2adf6f4281aa7d8
Mamba55013 <zghan2005@gmail.com>
2020-10-07 21:18:58 -0700

    Fixed game over function

--- client/game.py
+++ client/game.py
@@ -128,51 +128,6 @@ class Game:
     def add_to_hold(self):
         preview_piece
 
-
-    def game_over(self):
-
-        global bag, next_bag, rotations, speedLevel, current
-
-        gameOver = True
-
-        pygame.mixer.music.set_volume(0.03)
-        
-        while gameOver:
-            #Game over loop
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-
-                if event.type == pygame.KEYDOWN:
-
-                    if event.key == pygame.K_d:
-                        gameOver = False
-                        game.resting.clear()
-                        game.removing.clear()
-                        speedLevel = 0
-                        bag = pieces.copy()
-                        random.shuffle(bag)
-                        next_bag = pieces.copy()
-                        random.shuffle(next_bag)
-                        rotations = 0
-                        current = Piece(5, 1, bag.pop(0))
-                        pygame.mixer.music.set_volume(0.15)
-
-
-                
-
-            s = pygame.Surface((game.width, game.height), pygame.SRCALPHA) # noqa pylint: disable=too-many-function-args
-            s.fill((255,255,255, 2))      
-            game.screen.blit(s, (0,0))
-            game_over_font = pygame.font.Font('assets/arial.ttf', 60)
-            text = game_over_font.render(f'Game Over', True, (0, 0 ,0), game.screen)
-            textRect = text.get_rect() 
-            textRect.center = (game.width // 2, game.height // 2) 
-            game.screen.blit(text, textRect)
-            pygame.display.update()
-
-            game.clock.tick(60)
         
     
     def start_screen(self):
@@ -200,14 +155,15 @@ class Game:
                         
             self.screen.fill((0,0,0))
             
-            if new_piece.y >= 28:
-                new_piece.y = 1
 
             new_piece.render(False)
             if time.time() > last_fall:
                 new_piece.move(0, 1)
                 last_fall = time.time() + 1
-                print(new_piece.y)
+
+                if new_piece.check_floor():
+                    del new_piece
+                    new_piece = Piece(5, 2, 'T')
 
             pygame.display.update()
 

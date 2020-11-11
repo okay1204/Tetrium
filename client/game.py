4de36d9d52052d7a82ca19af10a8cf9682ce2683
VolVox99 <alirastegar123@gmail.com>
2020-11-11 12:39:24 -0800

    f

--- client/game.py
+++ client/game.py
@@ -166,19 +166,30 @@ class Game:
         self.background_color = theme[1]
         self.foreground_color = theme[2]
         self.set_grid_color(self.foreground_color)
+        self.set_text_color(self.foreground_color)
         self.theme_text = theme[0]
-        self.text_color = self.foreground_color
-        self.preview_and_meter_outline_color = (0, 0, 0)
-
-        self.preview_color = self.preview_and_meter_outline_color
-
+       
+       
 
    
     def set_grid_color(self, color):
         self.grid_color = tuple(darken(i, 10) for i in color)
 
+    
+    def complimentary_color(self, color: tuple):
+        
+        def contrast(val):
+            return abs((val + 255/2) - 255)
+        
+
+
+        return tuple(contrast(i) for i in color)
+
     def set_text_color(self, color):
         self.text_color = color
+        contrast = self.complimentary_color(color)
+        self.preview_color = contrast
+        self.meter_outline_color = contrast
 
     def render(self, pieces=None, held=None):
         
@@ -275,7 +286,7 @@ class Game:
 
 
         # junk line meter
-        pygame.draw.rect(self.screen, self.preview_and_meter_outline_color, (32, 394, 36, 306))
+        pygame.draw.rect(self.screen, self.meter_outline_color, (32, 394, 36, 306))
         pygame.draw.rect(self.screen, self.foreground_color, (35, 397, 30, 300))
 
         meter_block = 0
--- client/test.py
+++ client/test.py
@@ -1,5 +1,11 @@
+def complimentary_color(color_0: tuple, color_1: tuple):
+    
 
-x = list([1,2,4] + [6, 46, 9])
+    def compliment(val, num):
+        if not num:
+            return abs((val + 120) - 360)
 
+        return abs((val + 240) - 360)
 
-print(x)
+    
+    

a0e2ed0e3e8703fbd98a8bca314d74a8e472ecbc
VolVox99 <alirastegar123@gmail.com>
2020-11-22 20:40:16 -0800

    fixed positioning more

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ large_image="tetrium_logo_512x512"
 
 
         opp_rendered_text = rematch_text_font.render(opp_rematch_text, True, game.foreground_color)
-        game.screen.blit(opp_rendered_text, (opponent_rect.x -  opponent_rect.width*1.5, int(game.height/2)+250))
+        game.screen.blit(opp_rendered_text, (opponent_rect.x -  0.75 * opponent_rect.width, int(game.height/2)+250))
 
     def draw_texts():
         game.screen.blit(you_text, (int(game.width/4)-30, int(game.height/2)+100))
